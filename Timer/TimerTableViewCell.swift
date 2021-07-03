941deb253f4a2438f5d04cd293764a34cc96de41
Assylzhan <asylzhanbeats@gmail.com>
2021-07-04 03:13:09 +0600

    First commit

---
+++ Timer/TimerTableViewCell.swift
@@ -0,0 +1,78 @@
+//
+//  TimerTableViewCell.swift
+//  Timer
+//
+//  Created by Assylzhan Nurlybekuly on 02.07.2021.
+//
+
+import UIKit
+protocol TimerTableViewCellDelegate {
+    func removeFinishedTimer(with index: String)
+}
+class TimerTableViewCell: UITableViewCell {
+    
+    var timer: Timer?
+    var delegate: TimerTableViewCellDelegate?
+    private let timerLabel : UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        return label
+    }()
+    private let timerName : UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    static let identifier = "TimerTableViewCell"
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        contentView.addSubview(timerLabel)
+        contentView.addSubview(timerName)
+    }
+   
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        timerName.frame = CGRect(x: 20, y: 0, width: contentView.frame.size.width/3, height: contentView.frame.size.height-2)
+        timerLabel.frame = CGRect(x: contentView.frame.size.width/3*2 , y: 0, width: contentView.frame.size.width/3, height: contentView.frame.size.height-2)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    static func calculate(model: TimerModel)->Double{
+        return Double((model.createdTime + model.duration) - Date().timeIntervalSince1970)
+    }
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.timer?.invalidate()
+        self.timer = nil
+        self.timerLabel.text = nil
+        self.timerName.text = nil
+    }
+    public func configure(with model: TimerModel){
+        
+        if timer == nil {
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {[weak self]timer in
+                guard let strongSelf = self else {return}
+                let newtime = TimerTableViewCell.calculate(model: model)
+                if newtime>0 {
+                    strongSelf.timerLabel.text = newtime.timeRemainingFormatted()
+                }else{
+                    // finished
+                    strongSelf.delegate?.removeFinishedTimer(with: model.id)
+                }
+            })
+        }
+        timerName.text = model.name
+    }
+}
+
+extension Double {
+    public func timeRemainingFormatted() -> String {
+        let duration = TimeInterval(self)
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .positional
+        formatter.allowedUnits = [ .minute, .second ]
+        formatter.zeroFormattingBehavior = [ .pad ]
+        return formatter.string(from: duration) ?? ""
+    }
+}
--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -6,14 +6,226 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+struct TimerModel {
+    let id: String
+    let name: String
+    let createdTime: TimeInterval
+    let duration: TimeInterval
 }
 
+class ViewController: UIViewController {
+    public static let dataFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.dateFormat = "MM-dd-yyyy HH-mm-ss"
+        formatter.timeZone = TimeZone(abbreviation: "UTC")
+        return formatter
+    }()
+    private let topView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .secondarySystemBackground
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.lightGray.cgColor
+        return view
+    }()
+    private let secondView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .secondarySystemBackground
+        return view
+    }()
+    private let secondLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Добавление таймеров"
+        label.font = .systemFont(ofSize: 15, weight: .light)
+        label.textAlignment = .left
+        label.textColor = .gray
+        return label
+    }()
+    private let secondLine : UIView = {
+        let line = UIView()
+        line.backgroundColor = .systemGray4
+        return line
+    }()
+    private let titleLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Мульти таймер"
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
+    private var data = [TimerModel]()
+    private let tableView = UITableView()
+    private var nameTextField : UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Название таймера"
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .secondarySystemBackground
+        return field
+    }()
+    private var secondsTextField : UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Время в секундах"
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .secondarySystemBackground
+        return field
+    }()
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Добавить", for: .normal)
+        button.backgroundColor = .secondarySystemBackground
+        button.setTitleColor(.link, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return button
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Мульти таймер"
+        view.backgroundColor = .systemBackground
+        view.addSubview(topView)
+        topView.addSubview(titleLabel)
+        view.addSubview(secondView)
+        secondView.addSubview(secondLabel)
+        secondView.addSubview(secondLine)
+        view.addSubview(nameTextField)
+        view.addSubview(secondsTextField)
+        view.addSubview(button)
+        view.addSubview(tableView)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        tableView.dataSource = self
+        tableView.tableHeaderView = createTableHeader()
+        
+        
+        tableView.backgroundColor = .secondarySystemBackground
+        
+    }
+    func createTableHeader()->UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 50))
+//        headerView.backgroundColor = .red
+        let label = UILabel()
+        label.text = "Таймеры"
+        label.textColor = .gray
+        label.frame  = CGRect(x: 20, y: 10, width: self.view.width, height: 50)
+        let line = UIView()
+        line.backgroundColor = .systemGray4
+        line.frame = CGRect(x: 0, y: headerView.bottom, width: headerView.width, height: 1)
+        headerView.addSubview(line)
+        headerView.addSubview(label)
+        return headerView
+    }
+    @objc private func buttonTapped(){
+        guard nameTextField.text != nil, secondsTextField.text != nil else {return}
+        guard let duration = secondsTextField.text?.convertToTimeInterval() else { return  }
+        let id = ViewController.dataFormatter.string(from: Date())
+        let newModel = TimerModel(id: id, name: nameTextField.text ?? "NO NAME", createdTime: Date().timeIntervalSince1970, duration: duration)
+        var pos = 0
+        for model in data {
+            if TimerTableViewCell.calculate(model: model) <= Double(duration) {
+                break
+            }
+            pos += 1
+        }
+        print("new id: \(id)")
+        data.insert(newModel, at: pos)
+        nameTextField.text = nil
+        secondsTextField.text = nil
+        tableView.reloadData()
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.register(TimerTableViewCell.self, forCellReuseIdentifier: TimerTableViewCell.identifier)
+        let width = view.frame.size.width
+        let height = view.frame.size.height
+        topView.frame =  CGRect(x: 0, y: 0, width: width, height: height/7)
+        titleLabel.frame =  CGRect(x: 0, y: topView.height/2, width: width, height: 52)
+        
+        secondView.frame =  CGRect(x: 0, y: topView.bottom+20, width: width, height: 52)
+        secondLabel.frame =  CGRect(x: 20, y: 10 ,width: width-20, height: 42)
+        secondLine.frame =  CGRect(x: 0, y:secondLabel.bottom, width: width, height: 1)
+        
+        nameTextField.frame = CGRect(x: 20, y: secondView.bottom+15, width: width-130, height: 42)
+        secondsTextField.frame = CGRect(x: 20, y: nameTextField.bottom+10, width: width-130, height: 42)
+        button.frame = CGRect(x: 10, y: secondsTextField.bottom + 20, width: width-30, height: 52)
+        tableView.frame = CGRect(x: 0, y: button.bottom+50, width: width, height: height-secondsTextField.frame.origin.y-20)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TimerTableViewCell.identifier) as! TimerTableViewCell
+        cell.configure(with: data[indexPath.row])
+        cell.delegate = self
+        return cell
+    }
+}
+
+extension ViewController: TimerTableViewCellDelegate {
+    func removeFinishedTimer(with index: String) {
+        var pos = 0
+        for model in data {
+            if model.id == index {
+                print("Found")
+                data.remove(at: pos)
+                tableView.reloadData()
+                break
+            }
+            pos += 1
+        }
+        
+    }
+    
+}
+
+extension UIView{
+    public var width: CGFloat{
+        return self.frame.size.width
+    }
+    public var height: CGFloat{
+        return self.frame.size.height
+    }
+    public var top: CGFloat{
+        return self.frame.origin.y;
+    }
+    public var bottom: CGFloat{
+        return self.frame.size.height + self.frame.origin.y;
+    }
+    public var left: CGFloat{
+        return self.frame.origin.x;
+    }
+    public var right: CGFloat{
+        return self.frame.origin.x + self.frame.size.width
+    }
+}
+extension String {
+    func convertToTimeInterval() -> TimeInterval {
+           guard self != "" else {
+               return 0
+           }
+
+           var interval:Double = 0
+
+        interval += Double(self) ?? 0
+
+           return interval
+       }
+}
